85d6d1a84bfa40ad1c4dc509517b7e32202a1a76
Nasim Reza <nasim8382@gmail.com>
2024-07-17 14:18:31 +0600

    movie controller file updaed

--- src/app/modules/movies/movie.controller.ts
+++ src/app/modules/movies/movie.controller.ts
@@ -12,46 +12,78 @@ const createMovie = async (req: Request, res: Response) => {
 };
 
 const getAllMovies = async (req: Request, res: Response) => {
-  const result = await MovieServices.getAllMoviesFromDB();
+  try {
+    const result = await MovieServices.getAllMoviesFromDB();
 
-  res.json({
-    success: true,
-    message: "Movies are retrieved successfully !",
-    data: result,
-  });
+    res.status(200).json({
+      success: true,
+      message: "Movies are fetched successfully !",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Could not fetch movies!",
+      error: error,
+    });
+  }
 };
 
 const getSingleMovie = async (req: Request, res: Response) => {
-  const { movieId } = req.params;
-  const result = await MovieServices.getSingleMovieFromDB(movieId);
+  try {
+    const { movieId } = req.params;
+    const result = await MovieServices.getSingleMovieFromDB(movieId);
 
-  res.json({
-    success: true,
-    message: "Movie is retrieved successfully !",
-    data: result,
-  });
+    res.json({
+      success: true,
+      message: "Movie is retrieved successfully !",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Could not fetch movie!",
+      error: error,
+    });
+  }
 };
 
 const deleteMovie = async (req: Request, res: Response) => {
-  const { movieId } = req.params;
-  const result = await MovieServices.deleteMovieFromDB(movieId);
+  try {
+    const { movieId } = req.params;
+    const result = await MovieServices.deleteMovieFromDB(movieId);
 
-  res.json({
-    success: true,
-    message: "Movie is deleted successfully !",
-    data: result,
-  });
+    res.json({
+      success: true,
+      message: "Movie is deleted successfully !",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Could not delete movie!",
+      error: error,
+    });
+  }
 };
 
 const updateMovie = async (req: Request, res: Response) => {
-  const { movieId } = req.params;
-  const result = await MovieServices.updateMovieIntoDB(movieId, req.body);
+  try {
+    const { movieId } = req.params;
+    const result = await MovieServices.updateMovieIntoDB(movieId, req.body);
 
-  res.json({
-    success: true,
-    message: "Movie is updated successfully !",
-    data: result,
-  });
+    res.json({
+      success: true,
+      message: "Movie is updated successfully !",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Could not update movie!",
+      error: error,
+    });
+  }
 };
 
 export const MovieControllers = {
